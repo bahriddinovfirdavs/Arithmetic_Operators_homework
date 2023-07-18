2d1a9c317ab0bfbc89297202cb405716f8dee4a6
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-07-18 21:40:45 +1030

    arithmetic_op01.py

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,8 +1,9 @@
 #Create a variable x and assign it the value of 10.
-
+x=int(input())
 #Create a variable y and assign it the value of 5.
-
+y=int(input())
 #Create a variable result and assign it the value of x + y.
-
+Q=x+y
 #Print the value of result.
-#'The result is:'
+print(Q)
+#'The result is:'C:
