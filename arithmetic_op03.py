2d597a7c40aac62e95ee56390f5cd5c17ecb9615
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-07-18 21:52:37 +1030

    arithmetic_op03.py

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,8 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+a=int(input())
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+Q=a%10
+P=a//10
+A=(Q*10)+P
 #Print the answer variable
+print(A)
