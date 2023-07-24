cae30c3e6d6285bf1c98c6cea2ce44af971423d5
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-07-25 08:50:37 +1030

    arithmetic_op06.py

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,9 @@
 #Create a variable called 'number' with data type int.
- 
+a=int(input())
 #Divide the number by 2 and assign number.
- 
+Q=a//2
 #Find the fraction of the number and assing to a variable called 'answer'.
- 
+A=Q/100
 #Print the answer.
- 
+print(A)
 #Print the number.
